b1b85ac398162e2653194f8962bc58badf3baf63
brickoh <reenacting127@gmail.com>
2022-01-05 18:41:41 +0000

    Initial commit

---
+++ src/main/java/me/ethan/bpunishments/bPunishments.java
@@ -0,0 +1,17 @@
+package me.ethan.bpunishments;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class bPunishments extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
